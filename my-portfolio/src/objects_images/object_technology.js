9446fe02973f84f619f2e0a3de4c16b04e4c900d
Guilherme Azevedo <guilhermedagama2021100@gmail.com>
2023-02-15 23:10:38 -0300

    Foi adicionado imagens e criado um arquivo para fazer uma lista de nomes e imagens das tecnologias que estudei

---
+++ my-portfolio/src/objects_images/object_technology.js
@@ -0,0 +1,40 @@
+import JavaScript from '../images/technology/js.png';
+import Html5 from '../images/technology/html5.png';
+import Css3 from '../images/technology/css3.png';
+import React from '../images/technology/react.png';
+import Node from '../images/technology/nodejs.png';
+import Jest from '../images/technology/jest.png';
+import TestingLibrary from '../images/technology/testing-library.jpeg';
+
+const objectTechnology = [
+    {
+        name: "JavaScript",
+        image: JavaScript,
+    },
+    {
+        name:"Html 5",
+        image: Html5,
+    },
+    {
+        name: "CSS 3",
+        image: Css3,
+    },
+    {
+        name: "React",
+        image: React,
+    },
+    {
+        name: "NodeJS",
+        image: Node,
+    },
+    {
+        name: "Jest",
+        image: Jest,
+    },
+    {
+        name: "Testing Library",
+        image: TestingLibrary,
+    },
+];
+
+export default objectTechnology;
