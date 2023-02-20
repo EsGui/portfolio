6f06e1c8e8620abd768f23f071b1a249cab9bc05
Guilherme Azevedo <guilhermedagama2021100@gmail.com>
2023-02-19 22:55:20 -0300

    feat: o footer foi ajustado no final da página

--- my-portfolio/src/components/headerhomepage/headerhomepage.js
+++ my-portfolio/src/components/headerhomepage/headerhomepage.js
@@ -4,6 +4,12 @@ function HeaderHomePage() {
     return (
         <div className="StyleHeaderHomePage">
             <h1>Portfólio Web</h1>
+            <ul>
+                <li>Sobre</li>
+                <li>Projetos</li>
+                <li>Tecnologias</li>
+                <li>Contatos</li>
+            </ul>
         </div>
     )
 }
