ff73051478decd0b90ddbca6c53e90c9a50de2ad
Guilherme Azevedo <guilhermedagama2021100@gmail.com>
2023-03-06 19:57:28 -0300

    feat: enviando alterações

--- my-portfolio/src/components/projects/projects.js
+++ my-portfolio/src/components/projects/projects.js
@@ -2,8 +2,10 @@ import './projects.css'
 
 function Projects() {
     return (
-        <div>
-            <h1>Testando área de projetos</h1>
+        <div className="DivFatherProjects">
+            <div>
+                <h1>Testando área de projetos</h1>
+            </div>
         </div>
     )
 }
--- my-portfolio/src/pages/homepage.js
+++ my-portfolio/src/pages/homepage.js
@@ -3,7 +3,7 @@ import BannerHomePage from "../components/bannerhomepage/bannerhomepage"
 import FooterHomePage from "../components/footerhomepage/footerhomepage"
 import HeaderHomePage from "../components/headerhomepage/headerhomepage"
 import Projects from "../components/projects/projects"
-import Stacks from "../components/stacks/stacks"
+/* import Stacks from "../components/stacks/stacks" */
 
 
 function HomePage() {
